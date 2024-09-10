14a52ae70cb36384bd6e5342f8e91054a6715e2e
rahul-1729 <rk9763981@gmail.com>
2024-09-10 22:58:15 +0530

    layout dobne

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen grid grid-cols-5">
+      <div className="col-span-1 bg-indigo-700">{/* add blue section */}</div>
+      <div className="col-span-4 bg-gray-100 grid">
+        <div className="grid grid-rows-10 gap-1">
+          <div className="row-span-1 grid grid-cols-5 gap-1">
+            <div className="col-span-3 bg-white">
+              {/* search section */}
+            </div>
+            <div className="col-span-2  bg-white">
+              {/* active section  */}
+            </div>
+          </div>
+          <div className="row-span-9 grid grid-cols-5 gap-1">
+            <div className="col-span-3  bg-white">
+              {/* scroll section */}
+            </div>
+            <div className="col-span-2  bg-white">
+              {/* message section */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
