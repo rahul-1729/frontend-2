3068f3d20966a9e1daeb90ed1e2effb69693c7c0
rahul-1729 <rk9763981@gmail.com>
2024-09-11 13:14:27 +0530

    added the menu section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
  
 import './App.css';
+import Blue from './components/Blue'
 
 function App() {
   return (
     <div className="w-screen h-screen grid grid-cols-5">
-      <div className="col-span-1 bg-indigo-700">{/* add blue section */}</div>
+      <div className="col-span-1 bg-indigo-700 grid">{/* add blue section */}
+      
+      <Blue/>
+        
+      </div>
       <div className="col-span-4 bg-gray-100 grid">
         <div className="grid grid-rows-10 gap-1">
           <div className="row-span-1 grid grid-cols-5 gap-1">
-            <div className="col-span-3 bg-white">
+            <div className="col-span-3 bg-white grid">
               {/* search section */}
             </div>
-            <div className="col-span-2  bg-white">
+            <div className="col-span-2  bg-white grid ">
               {/* active section  */}
             </div>
           </div>
           <div className="row-span-9 grid grid-cols-5 gap-1">
-            <div className="col-span-3  bg-white">
+            <div className="col-span-3  bg-white grid ">
               {/* scroll section */}
             </div>
-            <div className="col-span-2  bg-white">
+            <div className="col-span-2  bg-white grid">
               {/* message section */}
             </div>
           </div>
---
+++ src/components/Blue.js
@@ -0,0 +1,101 @@
+import React from 'react'
+
+
+const Blue = () => {
+    
+ 
+
+
+
+
+
+   const sidebarMenu =[
+    {
+       name:"Feed",
+       path:"M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
+       
+    },
+    {
+      name:"Stories",
+      path: "M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+      
+   },
+   {
+    name:"Friends",
+    path: "M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+    
+ },
+ {
+   name:"APIs",
+   path:"M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+   
+ },
+ {
+  name:"Subsription",
+  path:"M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
+  
+},
+{
+ name:"Settings",
+ path:"M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z"
+ 
+},
+{
+name:"Help & Support",
+path:"M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+}
+ 
+
+ ]
+
+  return (
+    <div className='grid grid-rows-10 gap-1 px-3'>
+      <div className='row-span-1 font-bold flex items-center gap-2'>
+        <div className='bg-white w-1/6 h-2/5 rounded-xl flex justify-center items-center'><h2 className='text-3xl text-indigo-700 sans-serif'>S</h2></div>
+        <h2 className='sans-serif text-3xl text-white'>slothui</h2>
+      </div>
+
+      <div className='row-span-9  grid grid-rows-12 gap-1'>
+        {/* list */}
+        <div className='row-span-1 flex justify-center items-center'>
+          <div className='w-full h-2/3 bg-indigo-500 border border-indigo-300 rounded-full flex items-center px-3 gap-2'>
+              <svg class="w-5 h-5 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+                <h2 className='text-white font-sans'>Search</h2>
+          </div>
+        </div>
+
+
+        {sidebarMenu.map((value)=>
+
+(<>
+    <div className='row-span-1 rounded-xl flex items-center px-3 text-white gap-1'>
+     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
+<path stroke-linecap="round" stroke-linejoin="round" d={value.path} />
+</svg>
+<div><h2 className='text-white font-sans font-bold'>{value.name}</h2></div>
+</div></>)
+        )}
+     
+    
+      {/* list ended */}
+        <div className='row-span-3 '></div>
+        <div className='row-span-1 flex justify-center items-center'>
+          <div className='w-full h-2/3 bg-indigo-500 border border-indigo-300 rounded-full flex items-center justify-between px-3 gap-2 text-white'>
+               
+                <h2 className='text-white font-sans'>Go Pro</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>
+
+          </div>
+        </div>
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default Blue
